89d4697143ade20ee02d954414b531ef17345825
FairFightKing <sosthenefruchard@gmail.com>
2020-05-31 14:25:11 +0200

    Props used in component + bgImg relativ

--- src/app/component/product-card-command/product-card-command.component.ts
+++ src/app/component/product-card-command/product-card-command.component.ts
@@ -1,12 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-product-card-command',
-  templateUrl: './product-card-command.component.html',
+  template: '<ion-card>' +
+      '    <ion-card-header style="background: url({{imgUrl}})">' +
+      '    </ion-card-header>' +
+      '    <ion-card-content>' +
+      '        <ion-card-title>{{name}}</ion-card-title>' +
+      '        <hr>' +
+      '        <div class="productConfig">' +
+      '            <ion-card-subtitle>{{size}}</ion-card-subtitle>' +
+      '            <ion-card-subtitle><span>{{price}} euros</span></ion-card-subtitle>' +
+      '        </div>' +
+      '    </ion-card-content>' +
+      '</ion-card>',
   styleUrls: ['./product-card-command.component.scss'],
 })
 export class ProductCardCommandComponent implements OnInit {
 
+  @Input() imgUrl:string
+  @Input() name:string
+  @Input() size:string
+  @Input() price:number
   constructor() { }
 
   ngOnInit() {}
